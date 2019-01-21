ff811b022e3c50b7040750c5d41078794c64608e
guillermo <guillermogarcia1195@gmail.com>
2019-01-21 02:05:13 +0100

    comienzo, probando las epxresiones

--- main.js
+++ main.js
@@ -2,5 +2,18 @@
 
 
 function validarFormulario(){
-    alert("lool");
+
+
+    var expresionRegularNombre = new RegExp('^[A-Za-z]{2,15}$');
+    //var expresionRegularNombre = new RegExp('[0-9]');
+    var nombre = document.getElementById("nombre").value;
+    if(expresionRegularNombre.test(nombre)){
+        alert("xd");
+    }else{
+        alert("error en el nombre");
+    }
+
+
+
+
 }
