644882b7ae40b02f6f1da10a48387b1e0be633a0
guillermo <guillermogarcia1195@gmail.com>
2019-01-29 23:08:57 +0100

    arreglados fallos y quitar y poner vistas

--- main.js
+++ main.js
@@ -15,51 +15,77 @@ function validarFormulario(){
     var contrasenia = document.getElementById("contrasenia").value;
     var contraseniaConfirmacion = document.getElementById("contraseniaConfirmacion").value;
     var todoCorrecto = true;
+    document.getElementById("nombreError").style.visibility = "hidden";
+    document.getElementById("apellidoError").style.visibility = "hidden";
+    document.getElementById("emailError").style.visibility = "hidden";
+    document.getElementById("fechaContratoError").style.visibility = "hidden";
+    document.getElementById("telefonoError").style.visibility = "hidden";
+    document.getElementById("salarioError").style.visibility = "hidden";
+    document.getElementById("contraseniaError").style.visibility = "hidden";
+    document.getElementById("contraseniaConfirmacionError").style.visibility = "hidden";
 
     if(!validarNombre(nombre)){
+        document.getElementById("nombreError").style.visibility = "visible";
         document.getElementById("nombreError").innerHTML = "Error en el nombre";
         todoCorrecto = false;
     }
     
     if(!validarNombre(apellido)){
+        document.getElementById("apellidoError").style.visibility = "visible";
         document.getElementById("apellidoError").innerHTML = "Error en el apellido";
         todoCorrecto = false;
     }
 
     if(!validarEmail(email)){
+        document.getElementById("emailError").style.visibility = "visible";
         document.getElementById("emailError").innerHTML = "Error en el correo";
         todoCorrecto = false;
     }
     
     if(!valirFecha(contratacion)){
+        document.getElementById("fechaContratoError").style.visibility = "visible";
         document.getElementById("fechaContratoError").innerHTML = "Error en la contrtacion";
         todoCorrecto = false;
     }
-
-    if(!valirTelefono(telefono)){
+    
+    if(!valirTelefono(telefono) && telefono != ""){
+        document.getElementById("telefonoError").style.visibility = "visible";
         document.getElementById("telefonoError").innerHTML = "Error en el telefono";
         todoCorrecto = false;
     }
 
-    if(sueldo < 858,55 || sueldo > 12.000){
+    if(parseInt(sueldo) < 858.55 || parseInt(sueldo) > 12000){
+        document.getElementById("salarioError").style.visibility = "visible";
         document.getElementById("salarioError").innerHTML = "Error en el salario";
         todoCorrecto = false;
     }
 
     if(!validarContrasenia(contrasenia)){
+        document.getElementById("contraseniaError").style.visibility = "visible";
         document.getElementById("contraseniaError").innerHTML = "error en el formato de la contraseña";
         todoCorrecto = false;
     }
 
     if(contrasenia != contraseniaConfirmacion){
-        document.getElementById("contraseniaConfirmacion").innerHTML = "las contraseñas no coinciden";
+        document.getElementById("contraseniaConfirmacionError").style.visibility = "visible";
+        document.getElementById("contraseniaConfirmacionError").innerHTML = "las contraseñas no coinciden";
         todoCorrecto = false;
     }
 
+    setCookie(cookieEmail, email);
+    setCookie(cookieContrasenia, contrasenia);
 
     if(todoCorrecto)
         document.formulario.submit();
     
+}
+
+function setCookie(cname, cvalue) {
+    var exdays = 0;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function validarNombre(nombre){
