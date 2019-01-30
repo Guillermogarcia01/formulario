b5c274f7bceeffbf1534e78960ec081ea79e1105
guillermo <guillermogarcia1195@gmail.com>
2019-01-30 18:05:49 +0100

    anidadio mas o menos todo

--- main.js
+++ main.js
@@ -28,49 +28,57 @@ function validarFormulario(){
 
     if(!validarNombre(nombre)){
         document.getElementById("nombreError").style.visibility = "visible";
-        document.getElementById("nombreError").innerHTML = "Error en el nombre";
+        document.getElementById("nombreError").innerHTML = "Error en el nombre, sólo puede tener entre 2 y 15 carácteres, no puede contener números.";
+        document.getElementById("nombreError").style = "color:#FF0000";
         todoCorrecto = false;
     }
     
     if(!validarNombre(apellido)){
         document.getElementById("apellidoError").style.visibility = "visible";
-        document.getElementById("apellidoError").innerHTML = "Error en el apellido";
+        document.getElementById("apellidoError").innerHTML = "Error en el apellido, sólo puede tener entre 2 y 15 carácteres, no puede contener números";
+        document.getElementById("apellidoError").style = "color:#FF0000";
         todoCorrecto = false;
     }
 
     if(!validarEmail(email)){
         document.getElementById("emailError").style.visibility = "visible";
-        document.getElementById("emailError").innerHTML = "Error en el correo";
+        document.getElementById("emailError").innerHTML = "Error en el correo. Debe ser un email válido del dominio de la empresa, ejemplo: pedro.sanchez@pufo.es";
+        document.getElementById("emailError").style = "color:#FF0000";
         todoCorrecto = false;
     }
     
     if(!valirFecha(contratacion) || contratacion == ""){
         document.getElementById("fechaContratoError").style.visibility = "visible";
-        document.getElementById("fechaContratoError").innerHTML = "Error en la contrtacion";
+        document.getElementById("fechaContratoError").innerHTML = "Error en la contrtacion. No puede ser anterior al año 2000 ni posterior a la fecha actual";
+        document.getElementById("fechaContratoError").style = "color:#FF0000";
         todoCorrecto = false;
     }
     
     if(!valirTelefono(telefono) && telefono != ""){
         document.getElementById("telefonoError").style.visibility = "visible";
-        document.getElementById("telefonoError").innerHTML = "Error en el telefono";
+        document.getElementById("telefonoError").innerHTML = "Error en el telefono. Debe ser un número válido español (9 dígitos que comienzan por 6 o 7)";
+        document.getElementById("telefonoError").style = "color:#FF0000";
         todoCorrecto = false;
     }
 
     if(parseInt(sueldo) < 858.55 || parseInt(sueldo) > 12000 || sueldo == "") {
         document.getElementById("salarioError").style.visibility = "visible";
-        document.getElementById("salarioError").innerHTML = "Error en el salario";
+        document.getElementById("salarioError").innerHTML = "Error en el salario. No puede ser menor que el salario mínimo (858,55€) ni mayor que el del CEO (12.000€)";
+        document.getElementById("salarioError").style = "color:#FF0000";
         todoCorrecto = false;
     }
 
     if(!validarContrasenia(contrasenia)){
         document.getElementById("contraseniaError").style.visibility = "visible";
-        document.getElementById("contraseniaError").innerHTML = "error en el formato de la contraseña";
+        document.getElementById("contraseniaError").innerHTML = "error en el formato de la contraseña. Para que sea válida debe tener al menos ocho caracteres y contener al menos una letra minúscula, una mayúscula, un número y un símbolo";
+        document.getElementById("contraseniaError").style = "color:#FF0000";
         todoCorrecto = false;
     }
 
     if(contrasenia != contraseniaConfirmacion){
         document.getElementById("contraseniaConfirmacionError").style.visibility = "visible";
         document.getElementById("contraseniaConfirmacionError").innerHTML = "las contraseñas no coinciden";
+        document.getElementById("contraseniaConfirmacionError").style = "color:#FF0000";
         todoCorrecto = false;
     }
 
@@ -85,11 +93,7 @@ function validarFormulario(){
 }
 
 function setCookie(cname, cvalue) {
-    var exdays = 1;
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";path=/";
 }
 
 function validarNombre(nombre){
